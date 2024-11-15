192b12e2df520b0f81ccb35be4105a8131e72c98
Zhang Hao <vxyu82505@gmail.com>
2024-11-16 04:18:08 +1100

    page separation implemented using PageHelper

--- src/main/java/com/emarket/market/Service/ProductService.java
+++ src/main/java/com/emarket/market/Service/ProductService.java
@@ -1,10 +1,15 @@
 package com.emarket.market.Service;
 
+import com.emarket.market.pojo.Product;
+import com.emarket.market.vo.ProductDetailVo;
 import com.emarket.market.vo.ProductVo;
 import com.emarket.market.vo.ResponseVo;
+import com.github.pagehelper.PageInfo;
 
 import java.util.List;
 
 public interface ProductService {
-    ResponseVo<List<ProductVo>> productList(Integer categoryId, Integer pageNum, Integer pageSize);
+    ResponseVo<PageInfo> productList(Integer categoryId, Integer pageNum, Integer pageSize);
+
+    ResponseVo<ProductDetailVo> detail(Integer productId);
 }
--- src/main/java/com/emarket/market/Service/impl/ProductServiceImpl.java
+++ src/main/java/com/emarket/market/Service/impl/ProductServiceImpl.java
@@ -4,8 +4,11 @@ import com.emarket.market.Service.CategoryService;
 import com.emarket.market.Service.ProductService;
 import com.emarket.market.dao.ProductMapper;
 import com.emarket.market.pojo.Product;
+import com.emarket.market.vo.ProductDetailVo;
 import com.emarket.market.vo.ProductVo;
 import com.emarket.market.vo.ResponseVo;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static com.emarket.market.enums.ProductStatusEnum.OFF_SALE;
+
 @Slf4j
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -25,10 +30,11 @@ public class ProductServiceImpl implements ProductService {
     private ProductMapper productMapper;
 
     @Override
-    public ResponseVo<List<ProductVo>> productList(Integer categoryId, Integer pageNum, Integer pageSize) {
+    public ResponseVo<PageInfo> productList(Integer categoryId, Integer pageNum, Integer pageSize) {
         Set<Integer> categoryIdSet = new HashSet<>();
         categoryService.findSubCategoryId(categoryId, categoryIdSet);
         categoryIdSet.add(categoryId);
+        PageHelper.startPage(pageNum, pageSize);
         List<Product> products = productMapper.selectByCategoryIdSet(categoryIdSet);
 
         List<ProductVo> productVoList = new ArrayList<>();
@@ -37,6 +43,7 @@ public class ProductServiceImpl implements ProductService {
             BeanUtils.copyProperties(product, productVo);
             productVoList.add(productVo);
         }
-        return ResponseVo.success(productVoList);
+        PageInfo<ProductVo> pageInfo = new PageInfo<>(productVoList);
+        return ResponseVo.success(pageInfo);
     }
 }
